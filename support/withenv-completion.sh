00a8d744223308c8a35352bfffbca4f8c1431f46
Mike Williams <mdub@dogbiscuit.org>
2013-05-13 21:57:40 +1000

    Make "withenv" a bash function, rather than a script.

--- support/withenv-completion.sh
+++
@@ -1,7 +0,0 @@
-_withenv_complete()
-{
-  cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(cd ${WITHENV_DIR-$HOME/.withenv}; ls "$cur"* 2> /dev/null) )
-}
-
-complete -o default -F _withenv_complete withenv
---
+++ withenv.bash
@@ -0,0 +1,52 @@
+withenv()
+{
+  # Load arguments
+  env_files=()
+  while [ $# -gt 0 ]; do
+    arg="$1"; shift
+    if [ "$arg" = '--' ]; then
+      break
+    fi
+    env_files+=($(_withenv_resolve "$arg"))
+  done
+
+  if [ $# = 0 ]; then
+
+    # Print the specified files
+    for f in ${env_files[@]}; do
+      echo "# $f"
+      cat "$f"
+      echo
+    done
+
+  else
+
+    # Source the specified files, then run a command
+    (
+      for f in ${env_files[@]}; do
+        source "$f"
+      done
+      eval "$@"
+    )
+
+  fi
+}
+
+_withenv_resolve() {
+  name="$1"
+  for f in "$name" "${WITHENV_DIR=$HOME/.withenv}/$name"; do
+    if [ -f "$f" ]; then
+      echo "$f"
+      return
+    fi
+  done
+  echo "withenv: No such file: $name" >&2
+}
+
+_withenv_complete()
+{
+  cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(cd ${WITHENV_DIR-$HOME/.withenv}; ls "$cur"* 2> /dev/null) )
+}
+
+complete -o default -F _withenv_complete withenv

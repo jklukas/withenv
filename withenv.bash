50ad5e23868c3a9105fbc82fa26f65d417c72e12
Jeff Klukas <jeff@klukas.net>
2015-12-01 20:25:58 -0500

    Many bug fixes and simplifications, plus usage documentation

--- withenv.bash
+++ withenv.bash
@@ -1,34 +1,77 @@
-# This file should be sourced so these functions are present
-# in the shell session.
+# This file should be sourced make these functions present in a shell session:
+#   source withenv.sh
 
 
 _withenv_usage()
 {
-    echo "usage: withenv blah blah" >&2
+    cat >&2 <<EOF
+usage: withenv [envfile ...] -- command [argument ...]
+
+Execute a command with a modified environment.
+
+The modified environment is defined by one or more "envfiles"
+which are simply shell scripts that withenv will source before
+executing COMMAND. Envfiles may optionally be encrypted to protect
+secret values on disk which need to be passed as environment variables.
+Any envfile ending in .asc or .gpg will be decrypted via gpg.
+It is assumed you have a gpgagent running with your private key loaded.
+
+Envfiles should be placed in a directory defined by WITHENV_DIR.
+If WITHENV_DIR is not set, \$HOME/.withenv is assumed. Explicit
+paths to envfiles outside WITHENV_DIR are also recognized.
+
+If WITHENV_ALLEXPORT is set to "true", the allexport shell option will
+be set before sourcing envfiles. Unless this is set, it is necessary
+in your envfiles to explicitly export any variables you wish to be
+available in the environment when COMMAND is executed.
+
+
+Example envfile (stored as \$HOME/.withenv/aws.sh):
+
+export AWS_ACCESS_KEY_ID='ABCDEFGHIJKLMNOPQRST'
+export AWS_SECRET_ACCESS_KEY='abcdefghijklmnopqrstuvwzyzabcdefghijklmn'
+
+
+Example invocations:
+
+# Basic usage
+withenv aws.sh othercreds.gpg -- aws s3 ls
+
+# Print out the modified environment to the terminal
+withenv aws.sh -- env
+
+# Launch a bash subshell where the modified environment persists
+withenv aws.sh -- bash
+EOF
 }
 
 
-withenv()
-{
+withenv() {
+# The enclosing parentheses spawn a subprocess so that environment
+# modifications are local to this function invocation.
+(
     # Construct an array of environment files.
     env_files=()
     while [ $# -gt 0 ]; do
         arg="$1"; shift
+        if [ "$arg" = '-h' ] || [ "$arg" = '--help' ]; then
+            _withenv_usage
+            exit 1
+        fi
         if [ "$arg" = '--' ]; then
             break
         fi
         env_files+=($(_withenv_resolve "$arg"))
     done
 
-    # If no command given, print usage and exit.
+    # If no command was given, print usage and exit.
     if [ $# = 0 ]; then
         _withenv_usage
-        return 1
+        exit 1
     fi
 
-    # Temporarily set the 'allexport' option if appropriate.
+    # Set the 'allexport' option if appropriate.
     if [ "$WITHENV_ALLEXPORT" = true ]; then
-        local must_unset_allexport=$(echo $SHELLOPTS | grep allexport || echo true)
         set -o allexport
     fi
 
@@ -41,14 +84,9 @@ withenv()
         fi
     done
 
-    # Unset the 'allexport' option if we previously set it.
-    if [ "$must_unset_allexport" = true ]; then
-        set +o allexport
-    fi
-
     # Run the command.
     "$@"
-}
+) }
 
 
 _withenv_resolve() {
@@ -68,15 +106,22 @@ _withenv_complete()
     local end_options
     local cur=${COMP_WORDS[COMP_CWORD]}
 
-    # If '--' has been specified, delegate completion to the given command.
+    # Check if '--' has already been specified.
     for (( i=1; i <= COMP_CWORD; i++ )); do
-        if [ "$end_options" = "true" ]; then
-            # Delegate completion to the given command.
-            # _command_offset is defined in the bash_completion package.
-            _command_offset $i 2> /dev/null
-            return
-        elif [[ ${COMP_WORDS[i]} == "--" ]]; then
-            end_options="true"
+        if [[ ${COMP_WORDS[i]} == "--" ]]; then
+            if [ $COMP_CWORD -eq $(( i + 1 )) ]; then
+                # We're in the middle of matching the command name.
+                COMPREPLY=($(compgen -A command -X "!${cur}*"))
+                return
+            elif [ $COMP_CWORD -gt $i ]; then
+                # A command has been specified, so delegate further completion
+                # to that command using _command_offset from the
+                # bash_completion package. If bash_completion isn't loaded
+                # in the current shell, then fail silently and let default
+                # completion take over.
+                _command_offset $(( i + 1 )) 2> /dev/null
+                return
+            fi
         fi
     done
 
@@ -88,12 +133,16 @@ _withenv_complete()
 
     # Offer a list of environment files as the reply.
     setopt nullglob 2> /dev/null || shopt -s nullglob
-    COMPREPLY=()
-    COMPREPLY+=($(echo "$cur"*.{sh,bash,gpg,asc}))
-    COMPREPLY+=($(cd ${WITHENV_DIR=$HOME/.withenv} && echo "$cur"*))
+    COMPREPLY=()  # Initialize array.
+    COMPREPLY+=("$cur"*.{sh,bash,zsh,gpg,asc})  # Add envfiles from PWD
+    COMPREPLY+=("${WITHENV_DIR=$HOME/.withenv}/$cur"*)  # Add envfiles from WITHENV_DIR
+    COMPREPLY=(${COMPREPLY[@]##*/})  # remove directory prefix from each element
+    COMPREPLY=(${COMPREPLY[@]/withenv.sh})  # remove withenv.bash from array
+
 }
 
 if [[ -n ${ZSH_VERSION-} ]]; then
+    # This is zsh rather than bash
     autoload bashcompinit
     bashcompinit
 fi
